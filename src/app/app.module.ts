e9d22d2a0cb80df33449777e710789a9eeb803ec
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-09-21 20:39:02 -0500

    bambu-news-repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// FIREBASE MODULES
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBLTaX4nZ0q_BB0sO0h9h8EG8fkt5-58dY',
+    authDomain: 'angular-bambu-news.firebaseapp.com',
+    databaseURL: 'https://angular-bambu-news.firebaseio.com',
+    projectId: 'angular-bambu-news',
+    storageBucket: 'angular-bambu-news.appspot.com',
+    messagingSenderId: '717099730458',
+    appId: '1:717099730458:web:0a8c5594f6d7d3534773a3',
+    measurementId: 'G-4BFSQ4Q9LK',
+  },
 };
 
 /*
